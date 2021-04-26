cb13cc1083cdd6a56a26f83dc36d0546a37d9b11
Alex Edwards <alexedwards@Alexs-MacBook-Pro.local>
2021-04-26 11:23:55 -0600

    home styled

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,10 +1,12 @@
 import { Route } from "react-router-dom";
 import Home from "./components/Home";
+import Nav from "./components/Nav";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      <Nav />
       <Route exact path="/" component={Home} />
     </div>
   );
--- front-end/src/components/Home.js
+++ front-end/src/components/Home.js
@@ -1,14 +1,24 @@
+import fit1 from "../components/images/fit1.jpeg";
+
 export default function Home() {
   return (
     <div className="main">
-      <div className="top">
-        <button>Login</button>
-        <button>Register</button>
+      <div className="top"></div>
+      <div className="image-container">
+        <img src={fit1} alt="" />
       </div>
-      <div className="main">
-        <button>Get Started</button>
+      <div className="textcont">
+        <p>Attend classes that are run by local instructors, anywhere!</p>
       </div>
-      <div className="bottom"></div>
+      <div className="btncontainer">
+        <button className="mainbtn">Join Now</button>
+      </div>
+
+      <div className="bottom">
+        <h3>
+          Already have an account? <a href="">Log In</a>
+        </h3>
+      </div>
     </div>
   );
 }
