9eb0c3ad75e52da906c9ce83e4d8481e7336a29a
Alex Edwards <alexedwards@Alexs-MacBook-Pro.local>
2021-04-26 14:26:42 -0600

    fixed routing

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,14 +1,21 @@
 import { Route } from "react-router-dom";
 import Home from "./components/Home";
+import Nav from "./components/Nav";
+import Choice from "./components/Choice";
 import "./App.css";
-import InstructorRegister from './components/Registration'
+import InstructorRegister from "./components/Registration";
+import ClientRegister from "./components/ClientRegister";
 
 function App() {
   return (
     <div className="App">
+      <Nav />
       <Route exact path="/" component={Home} />
-      <Route path='/register'>
+      <Route path="/register">
         <InstructorRegister />
+      </Route>
+      <Route path="/clientregister">
+        <ClientRegister />
       </Route>
       <Route exact path="/choice" component={Choice} />
     </div>
