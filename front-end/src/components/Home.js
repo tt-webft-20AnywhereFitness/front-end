164c82b0cc00c3bf846faa2e829a9bf030aa6fe3
Alex Edwards <alexedwards@Alexs-MacBook-Pro.local>
2021-04-26 09:50:00 -0600

    home-page

---
+++ front-end/src/components/Home.js
@@ -0,0 +1,14 @@
+export default function Home() {
+  return (
+    <div className="main">
+      <div className="top">
+        <button>Login</button>
+        <button>Register</button>
+      </div>
+      <div className="main">
+        <button>Get Started</button>
+      </div>
+      <div className="bottom"></div>
+    </div>
+  );
+}
