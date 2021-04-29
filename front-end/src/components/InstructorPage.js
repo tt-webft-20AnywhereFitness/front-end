9c809f8b77596334d62e6ee9833a3374ef27e6d3
MichaelMcFaddenjr <michaelpmcfaddenjr@gmail.com>
2021-04-29 13:16:26 -0600

    tyring to fix add class

--- front-end/src/components/InstructorPage.js
+++ front-end/src/components/InstructorPage.js
@@ -1,16 +1,9 @@
 import React from 'react';
-import { Link } from "react-router-dom";
-
 
 const InstructorPage = (props) => {
     
     return (
-        <div>
-            <h1>Instructor Page</h1>
-            <Link to="/add-class" className="addClassButton">
-                <button>Add a Class</button>
-            </Link>
-        </div>
+        <h1>Instructor Page</h1>
     );
 };
 
